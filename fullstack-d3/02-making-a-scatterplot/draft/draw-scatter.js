2d38fdf72041287cb4e8d53518ce66facda3393a
Dan Denney <dandenney@Dans-MacBook-Pro.local>
2020-02-08 14:30:05 -0500

    feat(fullstackd3): add scatterplots

--- fullstack-d3/02-making-a-scatterplot/draft/draw-scatter.js
+++ fullstack-d3/02-making-a-scatterplot/draft/draw-scatter.js
@@ -1,5 +1,116 @@
 async function drawScatter() {
-  // your code goes here
-  
+  // Checklist 1 of 7 : Access Data
+  let dataset = await d3.json("../../my_weather_data.json");
+
+  const xAccessor = d => d.dewPoint;
+  const yAccessor = d => d.humidity;
+  const colorAccessor = d => d.cloudCover;
+
+  // Checklist 2 of 7 : Create chart dimensions
+
+  const maxLength = d3.min([window.innerHeight * 0.9, window.innerWidth * 0.9]);
+
+  let dimensions = {
+    height: maxLength,
+    width: maxLength,
+    margin: {
+      bottom: 50,
+      left: 50,
+      right: 10,
+      top: 10
+    }
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  // Checklist 3 of 7 : Draw canvas
+
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("height", dimensions.height)
+    .attr("width", dimensions.width);
+
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  // Checklist 4 of 7 : Create scales
+
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice();
+
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0])
+    .nice();
+
+  const colorScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, colorAccessor))
+    .range(["skyblue", "darkslategrey"]);
+
+  // Checklist 5 of 7 : Draw data
+
+  const dots = bounds
+    .selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(xAccessor(d)))
+    .attr("cy", d => yScale(yAccessor(d)))
+    .attr("r", 5)
+    .attr("fill", d => colorScale(colorAccessor(d)));
+
+  // Checklist 6 of 7 : Draw peripherals
+
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
+
+  const xAxis = bounds
+    .append("g")
+    .call(xAxisGenerator)
+    .attr("class", "axis")
+    .style("transform", `translateY(${dimensions.boundedHeight}px)`);
+
+  const xAxisLabel = xAxis
+    .append("text")
+    .attr("x", dimensions.boundedWidth / 2)
+    .attr("y", dimensions.margin.bottom - 10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .html("Dew point (&deg;F)");
+
+  const yAxisGenerator = d3
+    .axisLeft()
+    .scale(yScale)
+    .ticks(4);
+
+  const yAxis = bounds
+    .append("g")
+    .call(yAxisGenerator)
+    .attr("class", "axis");
+
+  const yAxisLabel = yAxis
+    .append("text")
+    .attr("x", -dimensions.boundedHeight / 2)
+    .attr("y", -dimensions.margin.left + 10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .text("Relative humidity")
+    .style("transform", "rotate(-90deg)")
+    .style("text-anchor", "middle");
+
+  // Checklist 7 of 7 : Set up interactions
 }
-drawScatter()
+drawScatter();
