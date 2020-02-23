3f4b6bf002e648baad0275659e777d6fe31baf38
Dan <dd.subscriptions@gmail.com>
2020-02-22 23:28:22 -0500

    feat(fullstack): add the final graphs

--- fullstack-d3/13-using-d3-with-react-js/src/completed/Chart/Axis-naive.jsx
+++ fullstack-d3/13-using-d3-with-react-js/src/completed/Chart/Axis-naive.jsx
@@ -1,46 +1,45 @@
-import React, { useRef } from "react"
-import PropTypes from "prop-types"
-import * as d3 from 'd3'
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import * as d3 from "d3";
 import { useChartDimensions } from "./Chart";
 
 const Axis = ({ dimension, scale, formatTick, ...props }) => {
-  const dimensions = useChartDimensions()
+  const dimensions = useChartDimensions();
 
   const axisGeneratorsByDimension = {
     x: "axisBottom",
-    y: "axisLeft",
-  }
+    y: "axisLeft"
+  };
   const axisGenerator = d3[axisGeneratorsByDimension[dimension]]()
     .scale(scale)
-    .tickFormat(formatTick)
+    .tickFormat(formatTick);
 
-  const ref = useRef()
+  const ref = useRef();
   if (ref.current) {
     d3.select(ref.current)
       .transition()
-      .call(axisGenerator)
+      .call(axisGenerator);
   }
 
   return (
-      <g {...props}
-        ref={ref}
-        transform={
-          dimension === "x"
-            ? `translate(0, ${dimensions.boundedHeight})`
-            : null
-        }
-      />
-  )
-}
+    <g
+      {...props}
+      ref={ref}
+      transform={
+        dimension === "x" ? `translate(0, ${dimensions.boundedHeight})` : null
+      }
+    />
+  );
+};
 
 Axis.propTypes = {
   dimension: PropTypes.oneOf(["x", "y"]),
-  scale: PropTypes.func,
-}
+  scale: PropTypes.func
+};
 
 Axis.defaultProps = {
   dimension: "x",
-  scale: null,
-}
+  scale: null
+};
 
-export default Axis
+export default Axis;
